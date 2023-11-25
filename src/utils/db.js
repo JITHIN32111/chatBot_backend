0aeda4768a34620b71da6ea1cb9234ad779d9887
Jithin C <jith97972@gmail.com>
2023-11-25 16:46:17 +0530

    changed mongo url

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,13 @@
+import { Long } from "mongodb";
 import mongoose from "mongoose";
 export const dbConnection = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      });
+      }).then((res)=>{
+      console.log("sucess");
+      })
     } catch (error) {
       console.error('Error connecting to the database:', error);
       process.exit(1);
