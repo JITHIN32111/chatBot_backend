d80f5e24849549c4aca83d8730fbfb9c83799a46
Jithin C <jith97972@gmail.com>
2023-11-25 21:04:36 +0530

    route changed /user

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ app.use(urlencoded({ extended: true }));
 dbConnection();
 handleSocketConnection(io)
 const BASE_URL = 'https://chatbotbackend-zsp1.onrender.com';
+app.use("/user",routes);
 app.use("/", async (req, res) => {
   res.send('Hello from /user');
 });
-app.use("/user",routes);
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 server.listen(PORT, () => {
