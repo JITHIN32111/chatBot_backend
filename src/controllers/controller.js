701b331ad78eafe25e85e82ec9acee848618c8ac
Jithin C <jith97972@gmail.com>
2023-11-25 20:25:25 +0530

    server testing

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,11 +3,27 @@ import Message from '../models/messageModel.js';
 export const getDataFromDb =async (req, res) => {
     try {
       const data = await Message.find().sort({ _id: -1 }).limit(10);
-      return res.status(201).json({
-          data
-      });
+      const html = generateHTML(data); // Function to generate HTML from data
+        res.send(html);
     } catch (error) {
       console.log(error);
       return res.status(500).json(error);
     }
   }
+  const generateHTML = (data) => {
+    const items = data.map(item => `<li>${item.someProperty}</li>`).join('');
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Data Page</title>
+        </head>
+        <body>
+            <h1>Data from Server</h1>
+            <ul>${items}</ul>
+        </body>
+        </html>
+    `;
+};
